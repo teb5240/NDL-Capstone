17b8e6e7c89ce44eab8b26d211ee2120328be947
TBaum <tbaum@mit.edu>
2017-11-12 19:29:27 -0500

    Code Simplification

---
+++ Read_data.m
@@ -0,0 +1,20 @@
+clc
+close all
+clear
+
+%% Read in EEG g.Nautilus data from Andrew's systems
+cd 'C:\Users\tbaum\OneDrive\Documents\NDL Capstone\Data';
+filename = 'C09S001R03.dat';
+
+[data_eeg,B,C,D] = load_bcidat(filename,'-calibrated');
+
+%% Filter EEG data
+
+samplingRate = 256;
+timeLength = length(data_eeg);
+time = 1:timeLength;
+
+figure
+plot(time, data_eeg(:,2))
+hold on
+xticks(0:10*samplingRate:timeLength)
